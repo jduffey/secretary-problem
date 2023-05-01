7c3976a1f5fe03012162b6a1b9ca0c4ab5e10642
Jed Duffey <jedduffey@gmail.com>
2023-05-01 14:41:59 -0400

    App now display a 2D chart showing results of simulation

--- src/App.js
+++ src/App.js
@@ -1,65 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Button, Row, Col, ProgressBar } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Line } from "react-chartjs-2";
+import { Chart } from "chart.js";
+import { CategoryScale } from "chart.js/auto";
+
+Chart.register(CategoryScale);
 
 const App = () => {
-    const [candidates, setCandidates] = useState([]);
-    const [bestCandidate, setBestCandidate] = useState(null);
-    const [selectedCandidate, setSelectedCandidate] = useState(null);
-    const [currentState, setCurrentState] = useState("start");
+    const numCandidates = 100;
+    const numSimulations = 1000;
 
-    const generateCandidates = () => {
-        const numCandidates = 100;
-        const newCandidates = Array.from({ length: numCandidates }, () => Math.floor(Math.random() * 100) + 1);
-        setCandidates(newCandidates);
-        setBestCandidate(Math.max(...newCandidates));
-        setSelectedCandidate(null);
-        setCurrentState("interview");
+    const [chartData, setChartData] = useState({});
+
+    const calculateSuccessRatio = (stopFraction) => {
+        let successCount = 0;
+
+        for (let i = 0; i < numSimulations; i++) {
+            const candidates = Array.from({ length: numCandidates }, () => Math.random());
+            const stopIndex = Math.floor(numCandidates * stopFraction);
+
+            const maxInFirstPhase = Math.max(...candidates.slice(0, stopIndex));
+            const remainingCandidates = candidates.slice(stopIndex);
+
+            const chosenCandidate = remainingCandidates.find((candidate) => candidate > maxInFirstPhase);
+            const bestCandidate = Math.max(...candidates);
+
+            if (chosenCandidate === bestCandidate) {
+                successCount++;
+            }
+        }
+
+        return successCount / numSimulations;
     };
 
-    const selectCandidate = (index) => {
-        setSelectedCandidate(candidates[index]);
-        setCurrentState("result");
+    const prepareChartData = () => {
+        const dataPoints = Array.from({ length: 101 }, (_, i) => i / 100);
+        const successRatios = dataPoints.map((stopFraction) => calculateSuccessRatio(stopFraction));
+
+        setChartData({
+            labels: dataPoints,
+            datasets: [
+                {
+                    label: "Success Ratio",
+                    data: successRatios,
+                    backgroundColor: "rgba(75, 192, 192, 0.6)",
+                    borderColor: "rgba(75, 192, 192, 1)",
+                    borderWidth: 2,
+                },
+            ],
+        });
     };
 
-    const optimalStop = Math.ceil(candidates.length * 0.368);
+    useEffect(() => {
+        prepareChartData();
+    }, []);
 
     return (
         <Container>
             <h1>Secretary Problem Simulator</h1>
-
-            {currentState === "start" && (
-                <Button onClick={generateCandidates}>Generate Candidates</Button>
-            )}
-
-            {currentState === "interview" && (
-                <>
-                    <h2>Candidates</h2>
-                    <Row>
-                        {candidates.map((candidate, index) => (
-                            <Col key={index}>
-                                <Button
-                                    variant={index < optimalStop ? "secondary" : "primary"}
-                                    onClick={() => selectCandidate(index)}
-                                >
-                                    {candidate}
-                                </Button>
-                            </Col>
-                        ))}
-                    </Row>
-                    <p>Optimal stopping point: Candidate {optimalStop}</p>
-                </>
-            )}
-
-            {currentState === "result" && (
-                <>
-                    <h2>Selected Candidate: {selectedCandidate}</h2>
-                    <h2>Best Candidate: {bestCandidate}</h2>
-                    <ProgressBar>
-                        <ProgressBar variant="success" now={(selectedCandidate / bestCandidate) * 100} />
-                    </ProgressBar>
-                    <Button onClick={generateCandidates}>Try Again</Button>
-                </>
+            {chartData.labels && chartData.datasets && (
+                <Line
+                    data={chartData}
+                    options={{
+                        scales: {
+                            x: {
+                                type: "category",
+                                title: {
+                                    display: true,
+                                    text: "Stopping Point",
+                                },
+                            },
+                            y: {
+                                title: {
+                                    display: true,
+                                    text: "Success Ratio",
+                                },
+                                min: 0,
+                                max: 1,
+                            },
+                        },
+                    }}
+                />
             )}
         </Container>
     );
