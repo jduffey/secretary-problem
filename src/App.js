38ddf76c44a68eef8a21e7dbfb5c4e0819c1e7ca
Jed Duffey <jedduffey@gmail.com>
2023-05-01 14:24:44 -0400

    Add first pass at App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button, Row, Col, ProgressBar } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [candidates, setCandidates] = useState([]);
+    const [bestCandidate, setBestCandidate] = useState(null);
+    const [selectedCandidate, setSelectedCandidate] = useState(null);
+    const [currentState, setCurrentState] = useState("start");
+
+    const generateCandidates = () => {
+        const numCandidates = 100;
+        const newCandidates = Array.from({ length: numCandidates }, () => Math.floor(Math.random() * 100) + 1);
+        setCandidates(newCandidates);
+        setBestCandidate(Math.max(...newCandidates));
+        setSelectedCandidate(null);
+        setCurrentState("interview");
+    };
+
+    const selectCandidate = (index) => {
+        setSelectedCandidate(candidates[index]);
+        setCurrentState("result");
+    };
+
+    const optimalStop = Math.ceil(candidates.length * 0.368);
+
+    return (
+        <Container>
+            <h1>Secretary Problem Simulator</h1>
+
+            {currentState === "start" && (
+                <Button onClick={generateCandidates}>Generate Candidates</Button>
+            )}
+
+            {currentState === "interview" && (
+                <>
+                    <h2>Candidates</h2>
+                    <Row>
+                        {candidates.map((candidate, index) => (
+                            <Col key={index}>
+                                <Button
+                                    variant={index < optimalStop ? "secondary" : "primary"}
+                                    onClick={() => selectCandidate(index)}
+                                >
+                                    {candidate}
+                                </Button>
+                            </Col>
+                        ))}
+                    </Row>
+                    <p>Optimal stopping point: Candidate {optimalStop}</p>
+                </>
+            )}
+
+            {currentState === "result" && (
+                <>
+                    <h2>Selected Candidate: {selectedCandidate}</h2>
+                    <h2>Best Candidate: {bestCandidate}</h2>
+                    <ProgressBar>
+                        <ProgressBar variant="success" now={(selectedCandidate / bestCandidate) * 100} />
+                    </ProgressBar>
+                    <Button onClick={generateCandidates}>Try Again</Button>
+                </>
+            )}
+        </Container>
+    );
+};
 
 export default App;
