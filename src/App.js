586c9fef47a190ed516ebc65737ebfb23491dabb
Jed Duffey <jedduffey@gmail.com>
2023-05-09 18:35:57 -0400

    Move color scheme code and color legend component to SimulationManager

--- src/App.js
+++ src/App.js
@@ -3,27 +3,12 @@ import { Container, Col, Row } from "react-bootstrap";
 
 import { Introduction } from "./components/Introduction";
 import { Summary } from "./components/Summary";
-import { ColorLegend } from "./components/ColorLegend";
 import { ChartExplanation } from "./components/ChartExplanation";
 import { SimulationManager } from "./components/SimulationManager";
-
-import colorSchemes from "./colorSchemes";
 
 const NUM_CANDIDATES = 1000;
 const NUM_STOPPING_POINTS = NUM_CANDIDATES;
 const NUM_SIMULATIONS = 100_000;
-const COLOR_SCHEME_NAME = {
-    0: "Standard Colors",
-    1: "HAL 9000",
-    2: "Sunspot",
-}[2];
-const COLOR_SCHEME = {
-    thresholds:
-        Object.entries(colorSchemes[COLOR_SCHEME_NAME].thresholds)
-            .sort((a, b) => a.threshold - b.threshold)
-            .map(([threshold, color]) => ({ threshold: parseFloat(threshold), color })),
-    defaultColor: colorSchemes[COLOR_SCHEME_NAME].defaultColor,
-};
 
 const App = () => {
     return (
@@ -41,18 +26,7 @@ const App = () => {
                     numSimulations={NUM_SIMULATIONS}
                     numCandidates={NUM_CANDIDATES}
                     numStoppingPoints={NUM_STOPPING_POINTS}
-                    colorScheme={COLOR_SCHEME}
                 />
-            </Row>
-            <Row>
-                {
-                    COLOR_SCHEME &&
-                    <Col>
-                        <ColorLegend
-                            colorScheme={COLOR_SCHEME}
-                        />
-                    </Col>
-                }
             </Row>
             <Row>
                 <ChartExplanation
