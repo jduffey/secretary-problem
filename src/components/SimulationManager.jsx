4ce69ac2cb8169dfafe130794a453d4283fbab41
Jed Duffey <jedduffey@gmail.com>
2023-05-13 15:52:26 -0400

    DS fix

--- src/components/SimulationManager.jsx
+++ src/components/SimulationManager.jsx
@@ -77,7 +77,8 @@ export const SimulationManager = ({ numCandidates, numStoppingPoints, numSimulat
         for (let i = 0; i < numCandidates; i++) {
             hash = await window.crypto.subtle.digest('SHA-256', hash);
             const hashArray = Array.from(new Uint8Array(hash));
-            const bigIntHash = BigInt('0x' + hashArray.map(b => b.toString(16).padStart(2, '0')).join(''));
+            const hexValue = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+            const bigIntHash = BigInt(`0x${hexValue}`);
             candidates.push(bigIntHash);
             hash = new TextEncoder().encode(bigIntHash.toString());
         }
